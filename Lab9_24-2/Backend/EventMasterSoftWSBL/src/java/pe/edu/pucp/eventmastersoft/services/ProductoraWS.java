11e82bc1ba48995516324180fec1ff9339cba779
rio <rio88920@gmail.com>
2025-05-28 17:45:37 -0500

    lab 9

---
+++ Lab9_24-2/Backend/EventMasterSoftWSBL/src/java/pe/edu/pucp/eventmastersoft/services/ProductoraWS.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
+ */
+package pe.edu.pucp.eventmastersoft.services;
+
+import jakarta.jws.WebService;
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+
+import java.util.ArrayList;
+
+import pe.edu.pucp.eventmastersoft.dao.ProductoraDAO;
+import pe.edu.pucp.eventmastersoft.model.Productora;
+import pe.edu.pucp.eventmastersoft.mysql.ProductoraMySQL;
+
+@WebService(serviceName = "ProductoraWS", targetNamespace =
+"http://services.eventmastersoft.pucp.edu.pe")
+public class ProductoraWS {
+    private ProductoraDAO daoProductora;
+    
+    @WebMethod(operationName = "listarTodasProductoras")
+    public ArrayList<Productora> listarTodasProductoras() {
+        ArrayList<Productora> productoras = null;
+        
+        try{
+            daoProductora = new ProductoraMySQL();
+            productoras = daoProductora.listarTodas();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        
+        return productoras;
+    }
+}
